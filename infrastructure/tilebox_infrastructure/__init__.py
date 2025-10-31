753f0059bf7f5251464a6ff1aea24544ea068cfe
Corentin <33163342+corentinmusard@users.noreply.github.com>
2025-10-31 09:50:21 +0100

    Use tilebox-iac package (#2)

--- infrastructure/tilebox_infrastructure/__init__.py
+++
@@ -1,5 +0,0 @@
-from tilebox_infrastructure.auto_scaling_cluster import AutoScalingGCPCluster
-from tilebox_infrastructure.image_builder import LocalBuildTrigger
-from tilebox_infrastructure.secrets import Secret
-
-__all__ = ["AutoScalingGCPCluster", "LocalBuildTrigger", "Secret"]
